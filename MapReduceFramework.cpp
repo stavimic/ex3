731db069277da2d08730d98f9fc554e96260ee01
hareld10 <harel91827364>
2018-05-27 16:48:34 +0300

    1

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -5,20 +5,21 @@
 
 
 
+void emit2 (K2* key, V2* value, void* context){
+   
+}
 
 
-
-
-
-
-
-
-void emit2 (K2* key, V2* value, void* context){}
 void emit3 (K3* key, V3* value, void* context){}
 
 
 void runMapReduceFramework(const MapReduceClient& client,
 	const InputVec& inputVec, OutputVec& outputVec,
-	int multiThreadLevel){}
+	int multiThreadLevel) {
+
+
+
+
+}
 
 
