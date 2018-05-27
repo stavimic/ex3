49ff55acf1e89645a38b3f5cb5a2e7907eeed5cc
mstav <stav.michaely@mail.huji.ac.il>
2018-05-27 10:58:06 +0300

    New project

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,12 @@
+#include "MapReduceFramework.h"
+#include "MapReduceClient.h"
+
+void emit2 (K2* key, V2* value, void* context){}
+void emit3 (K3* key, V3* value, void* context){}
+
+
+void runMapReduceFramework(const MapReduceClient& client,
+	const InputVec& inputVec, OutputVec& outputVec,
+	int multiThreadLevel){}
+
+
